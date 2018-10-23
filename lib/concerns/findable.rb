94a996f57ecbb869075183047e1004daed1f15d1
Rose Brennan <brennan.m.rose@gmail.com>
2018-10-23 02:37:09 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,14 +1,6 @@
 module Findable
   
-  # def self.find_by_name
-  #   self.all.detect{|a| a.name == name}
-  # end 
-  
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-  
-    def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  def self.find_by_name
+    self.all.detect{|a| a.name == name}
+  end 
 end
